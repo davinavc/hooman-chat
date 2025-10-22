678297d1ccaaf53359fb5bd704538be7fdd14da5
davina <davinavc18@gmail.com>
2025-10-22 21:09:16 +0700

    create message_user table

---
+++ resources/js/Pages/index.jsx
@@ -0,0 +1,34 @@
+import { Head } from '@inertiajs/react';
+
+export default function Welcome({ auth }) {
+    return (
+        <>
+            <Head title="Welcome" />
+            <div className="flex">
+                <div className="w-2/6 bg-gray-900 border-r border-gray-800">
+                    <div className="pt-4 flex flex-col h-screen">
+                        <div className="px-4">
+                            <div className="flex justify-between mb-2">
+                                <h1 className="text-white text-xl font-bold">
+                                    Hooman Chat
+                                </h1>
+                                <div>
+                                    # Settings
+                                </div>
+                            </div>
+                            # search #
+                        </div>
+                        <div className="flex-1 px-4 overflow-y-auto">
+                            # room #
+                        </div>
+                    </div>
+                    <div className="relative w-4/6">
+                        # chat #
+                    </div>
+                </div>
+
+            </div>
+            
+        </>
+    );
+}
